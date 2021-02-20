5545a24df31c835dcb7f3651cfe0134fdb7165b1
Finsoy <vadimka.cry.00@gmail.com>
2021-02-20 21:14:18 +0300

    feat: add min and max function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    let minValue = 0;
+    if (array) {
+        for (let i = 0; i <= array.length - 1; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let maxValue = 0;
+    if (array) {
+        for (let i = 0; i <= array.length - 1; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (array) {
+        for (let i = 0; i <= array.length - 1; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    } else return 0;
+};
